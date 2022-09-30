a9c3532c50c35c6c175f0cf2cfb507d667fce564
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-09-30 00:22:14 -0700

    Bug fix and minor tweaking

--- helpers.js
+++ helpers.js
@@ -3,16 +3,16 @@ export const parseArgs = (rawArgs) => {
   const cleanArgs = {};
 
   args.forEach((arg) => {
-    // Confirm the arg starts with '--' // ! BUG: This can be marked as 'valid': 'name--Chris'
-    const valid = /(--)\w/.test(arg);
+    // Confirm the arg starts with '--'
+    const valid = /^--\w/.test(arg);
     if (valid) {
       const [key, value] = arg.split('=');
 
-      // Disregard opening '--'
+      // Disregard opening '--' for key name
       const cleanKey = key.slice(2);
 
       // If value has been provided, use it, else default value to true
-      cleanArgs[cleanKey] = value ? value : true;
+      cleanArgs[cleanKey] = value || true;
     }
   })
   return cleanArgs;
